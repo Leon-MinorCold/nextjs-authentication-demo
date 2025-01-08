9a93d2bcc04f463ae22c785c01dd3caa4fbf377b
Leon <leonminorcold@outlook.com>
2025-01-08 09:41:56 +0800

    chore: update eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,24 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+	baseDirectory: __dirname,
+})
 
-export default [
-  ...compat.extends('next/core-web-vitals'),
-  ...compat.extends('prettier'),
-  ...compat.plugins('prettier'),
-  {
-    rules: {
-      'prettier/prettier': 'error',
-      // '@typescript-eslint/no-unused-vars': 'warn',
-      'react-hooks/exhaustive-deps': 'warn',
-      // '@typescript-eslint/no-explicit-any': 'warn',
-      // 其他规则...
-    },
-  },
-];
+const config = [
+	...compat.extends('next/core-web-vitals'),
+	...compat.extends('prettier'),
+	...compat.plugins('prettier'),
+	{
+		rules: {
+			'prettier/prettier': 'error',
+			'react-hooks/exhaustive-deps': 'warn',
+		},
+	},
+]
+
+export default config
